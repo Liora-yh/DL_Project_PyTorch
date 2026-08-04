32196f0c629ca21058b6398aaf214cfe5c708fb5
落雨花 <yuhualuo043@gmail.com>
2026-08-04 18:29:49 +0800

    模型训练

--- day07/03_RNN案例_AI歌词生成器.py
+++ day07/03_RNN案例_AI歌词生成器.py
@@ -1,7 +1,6 @@
 import torch
 import re
 import jieba
-from sympy.matrices.expressions import transpose
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
@@ -103,7 +102,7 @@ class TextGenerator(nn.Module):
         # print(f'嵌入层处理后的维度：{embd.shape}')
         # rnn 处理
         # rnn格式：(句子的长度，batch句子的数量，隐藏层维度)
-        output, hidden = self.rnn(embd, transpose(0, 1), hidden)
+        output, hidden = self.rnn(embd.transpose(0, 1), hidden)
         # print(f'循环网络层处理后的维度：{output.shape}')
         # print(f'隐藏层处理后的维度：{hidden.shape}')
         # 全连接，输入内容必须是二维数据，即：词的数量 * 次的维度
@@ -117,16 +116,60 @@ class TextGenerator(nn.Module):
     # 隐藏层的初始化方法
     def init_hidden(self, bs):      # batch_size
         # 隐藏层初始化：[网络层数，batch，隐藏层向量维度]
-        return torch.zero(1, bs, 256)
+        return torch.zeros(1, bs, 256)
 
 # 训练模型
+def train():
+    # 构建词典
+    unique_words, word_to_index, unique_word_count, corpus_idx = build_vocab()
+    # 获取数据集
+    lyrics = LyricsDataset(corpus_idx, 32)
+    # 初始化（神经网络）模型
+    model = TextGenerator(unique_word_count)        # 预测5703个个词，每个词的概率
+    # 创建数据加载器
+    # 参1：数据集对象，参2：批次大小（每批5个句子，每个句子32个词），参3：是否打乱数据集
+    lyrics_dataloader = DataLoader(lyrics, batch_size=5, shuffle=True)
+    # 定义损失函数
+    criterion = nn.CrossEntropyLoss()
+    # 定义优化器
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    # 模型训练
+    # 定义变量，记录训练的轮数
+    epochs = 10
+    # 具体的每轮训练动作
+    for epoch in range(epochs):
+        # 定义变量，记录本轮开始训练时间，迭代(批次)次数，训练总损失
+        start, iter_num, total_loss = time.time(), 0, 0.0
+        # 具体的本轮各批次训练动作
+        # 遍历数据集，后台回调用LyricsDtaset#__getitem__()方法，获取到每个样本的数据和标签
+        for x, y in lyrics_dataloader:
+            # 获取隐藏层初始值
+            hidden = model.init_hidden(5)
+            # 模型计算
+            output, hidden = model(x, hidden)
+            # 计算损失
+            # y的形状：（batch, seq_len, 词向量维度）——> 转成一维向量 ——> 每个词的下标索引
+            # output的形状：（seq_len, batch, 词向量维度）
+            y = torch.transpose(y, 0, 1).reshape(shape=(-1, ))
+            loss = criterion(output, y)
+            # 梯度清零+反向传播+更新参数
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            # 累计损失和迭代次数
+            total_loss += loss.item()
+            iter_num += 1
+        # 本轮训练结束，打印本轮的训练信息
+        print(f'epoch: {epoch+1}, item: {time.time() - start:.2f}s, loss: {total_loss / iter_num:.4f}')
+    # 多轮训练结束（模型训练结束），保存模型
+    torch.save(model.state_dict(), './model/text_generate.pth')
 
 # 模型预测
 
 if __name__ == '__main__':
     # build_vocab()
     # 1、获取数据，进行分词，获取词表
-    unique_words, word_to_index, word_count, corpus_idx = build_vocab()
+    # unique_words, word_to_index, word_count, corpus_idx = build_vocab()
     # print(f'词的数量：{word_count}')         # 去重后，5703个词
     # print(f'去重后的词：{unique_words}')
     # print(f'每个词的索引：{word_to_index}')
@@ -140,8 +183,11 @@ if __name__ == '__main__':
     # print(f'输入值：{x}')
     # print(f'目标值：{y}')
 
-    # 3、创建模型对象
-    model = TextGenerator(word_count)
-    # 查看参数
-    for name, parameter in model.named_parameters():
-        print(f'参数名称：{name}, 参数维度：{parameter.shape}')
+    # # 3、创建模型对象
+    # model = TextGenerator(word_count)
+    # # 查看参数
+    # for name, parameter in model.named_parameters():
+    #     print(f'参数名称：{name}, 参数维度：{parameter.shape}')
+
+    # 训练并保存模型
+    train()
