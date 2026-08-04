07c4977b040ebd41c04d7ea09fdec4a26858af3a
落雨花 <yuhualuo043@gmail.com>
2026-08-04 15:26:31 +0800

    搭建神经网络

--- day07/03_RNN案例_AI歌词生成器.py
+++ day07/03_RNN案例_AI歌词生成器.py
@@ -1,6 +1,7 @@
 import torch
 import re
 import jieba
+from sympy.matrices.expressions import transpose
 from torch.utils.data import DataLoader
 import torch.nn as nn
 import torch.nn.functional as F
@@ -82,6 +83,41 @@ class LyricsDataset(torch.utils.data.Dataset):
         return torch.tensor(x), torch.tensor(y)
 
 # 搭建RNN神经网络
+class TextGenerator(nn.Module):
+    # 初始化方法
+    def __init__(self,unique_word_count):   # unique_word_count: 去重的词的数量(703)
+        # 初始化父类的成员
+        super().__init__()
+        # 初始化词嵌入层：语料中词的数量，词向量的维度
+        self.ebd = nn.Embedding(unique_word_count,128)
+        # 循环网络层：词向量维度，隐藏层维度：256，网络层：1
+        self.rnn = nn.RNN(128, 256, 1)
+        # 输出层（全连接层）：特征向量维度（和隐藏层向量维度一致），词表中词的个数
+        self.out = nn.Linear(256, unique_word_count)   # 词表中每个词的概率 ——> 选概率最大的那个词作为预测结果
+
+    # 前向传播方法
+    def forward(self, inputs, hidden):
+        # 初始化 嵌入层处理
+        # embd格式：(batch句子的数量，句子的长度，词向量维度)
+        embd = self.ebd(inputs)
+        # print(f'嵌入层处理后的维度：{embd.shape}')
+        # rnn 处理
+        # rnn格式：(句子的长度，batch句子的数量，隐藏层维度)
+        output, hidden = self.rnn(embd, transpose(0, 1), hidden)
+        # print(f'循环网络层处理后的维度：{output.shape}')
+        # print(f'隐藏层处理后的维度：{hidden.shape}')
+        # 全连接，输入内容必须是二维数据，即：词的数量 * 次的维度
+        # output = self.out(output.transpose(0, 1).reshape(-1, 256))
+        # 输入维度：(seq_len句子数量 * batch，词向量维度256)
+        # 输出维度：(seq_len句子数量 * batch，词表中次的个数)
+        output = self.out(output.reshape(shape=(-1, output.shape[-1])))  # 同上
+        # 返回结果,预测结果，隐藏层
+        return output, hidden
+
+    # 隐藏层的初始化方法
+    def init_hidden(self, bs):      # batch_size
+        # 隐藏层初始化：[网络层数，batch，隐藏层向量维度]
+        return torch.zero(1, bs, 256)
 
 # 训练模型
 
@@ -89,17 +125,23 @@ class LyricsDataset(torch.utils.data.Dataset):
 
 if __name__ == '__main__':
     # build_vocab()
-    #获取数据，进行分词，获取词表
+    # 1、获取数据，进行分词，获取词表
     unique_words, word_to_index, word_count, corpus_idx = build_vocab()
     # print(f'词的数量：{word_count}')         # 去重后，5703个词
     # print(f'去重后的词：{unique_words}')
     # print(f'每个词的索引：{word_to_index}')
     # print(f'文档中每个词对应的索引：{corpus_idx}')
 
-    # 构建数据集
-    dataset = LyricsDataset(corpus_idx, 5)
-    print(f'句子数量：{len(dataset)}')
-    # 查看输入值和目标值
-    x, y = dataset[1]
-    print(f'输入值：{x}')
-    print(f'目标值：{y}')
+    # # 2、构建数据集
+    # dataset = LyricsDataset(corpus_idx, 5)
+    # print(f'句子数量：{len(dataset)}')
+    # # 查看输入值和目标值
+    # x, y = dataset[1]
+    # print(f'输入值：{x}')
+    # print(f'目标值：{y}')
+
+    # 3、创建模型对象
+    model = TextGenerator(word_count)
+    # 查看参数
+    for name, parameter in model.named_parameters():
+        print(f'参数名称：{name}, 参数维度：{parameter.shape}')
